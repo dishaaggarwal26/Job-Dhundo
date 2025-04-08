484b889ce394a140b30d2ace25f9bd44e5f0e74c
dishaaggarwal26 <aggarwaldisha26@gmail.com>
2025-04-08 10:56:27 +0530

    resolved conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Home from './Components/Home';
 import Signup from './Components/SignupLogin';
@@ -6,6 +5,7 @@ import LostPassword from './Components/LostPassword';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Job from'./Components/Job';
+import Profile from './Components/Profile';
 import { Route, Routes } from 'react-router-dom';
 
 
@@ -17,34 +17,8 @@ const  App=()=>{
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
-
         <Route path="/Job" element={<Job />} />
-=======
-import React from 'react'
-import Home from "./Components/Home";
-import Signup from "./Components/SignupLogin";
-import LostPassword from "./Components/LostPassword"
-import Profile from './Components/Profile';
-import { Route, Routes } from "react-router-dom";
-
-
-const App = () => {
-  return (
-    <div>
-
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Login" element={<Signup />} />
-        <Route path="/lost-password" element={<LostPassword />} />
         <Route path="/Profile" element={<Profile />} />
-      </Routes>
-    </div>
-  )
-}
->>>>>>> ad182f414dec03c87ddf4df1ec41a2b63a63efdb
-
         <Route path="/login" element={<Signup/>} />
         <Route path="/lost-password" element={<LostPassword />} />
       </Routes>
