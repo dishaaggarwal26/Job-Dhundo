6825fd2410a31bfc1095a500d53342476e802e51
PRANITA GUPTA <pranita2413@gmail.com>
2025-04-14 12:10:58 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import LostPassword from './Components/LostPassword';
 import Navbar from './Components/Navbar';
 import Profile from './Components/Profile'
 import Footer from './Components/Footer';
-import Job from'./Components/Job';
+import Job from './Components/Job';
 import Blogs from './Components/Blogs';
 import BlogDetail from './blog-component/BlogDetail';
-import Testimonial from './Components/Testimonial';
-import Notifications from'./Components/Notifications';
-import Settings from './Components/Settings'; 
+import NotificationPanel from './Components/NotificationPanel';
+import Settings from './Components/Settings';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import './App.css';
@@ -18,30 +17,28 @@ import PostCompany from './Components/PostCompany';
 import BlogPost from './Components/BlogPost';
 import Help from './Components/Help';
 
-const  App=()=>{
+const App = () => {
   return (
-<div className='overflow-x-hidden'>
-        <Navbar/>
-      <ScrollToTop/>
-    <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/Home" element={<Home/>} />
-        <Route path="/Jobs" element={<Job/>} />
+    <div className='overflow-x-hidden'>
+      <Navbar />
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Jobs" element={<Job />} />
         <Route path="/Profile" element={<Profile />} />
-        <Route path="/Blogs" element={<Blogs/>} />
-        <Route path="/new-blogpost" element={<BlogPost/>} />
-        <Route path="/Help" element={<Help/>} />
+        <Route path="/Blogs" element={<Blogs />} />
+        <Route path="/new-blogpost" element={<BlogPost />} />
+        <Route path="/Help" element={<Help />} />
         <Route path="/blog/:id" element={<BlogDetail />} />
-        <Route path="/Testimonial" element={<Testimonial />} />
-        <Route path="/Join" element={<Signup/>} />
-        <Route path="/Notifications" element={<Notifications/>} />
-
-        <Route path="/Companies" element={<PostCompany/>}/>
+        <Route path="/Join" element={<Signup />} />
+        <Route path="/NotificationPanel" element={<NotificationPanel />} />
+        <Route path="/Companies" element={<PostCompany />} />
         <Route path="/lost-password" element={<LostPassword />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
-      <Footer/>
-</div>
+      <Footer />
+    </div>
   );
 };
 
